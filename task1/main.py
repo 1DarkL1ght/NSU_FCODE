4b4cb5a8b9c70e3eb31f0c22a725083840cfc855
Dark L1ght <georgmaximov160805@gmail.com>
2025-10-02 13:40:05 +0700

    small fix

--- task1/main.py
+++ task1/main.py
@@ -1,15 +1,17 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import cv2
+import numpy as np
 from skimage.data import astronaut
 from jsonargparse import CLI
-
 
 def process_image(image: cv2.Mat, delta: float) -> cv2.Mat:
     """Changes S (saturation) channel in HSV image"""
 
     image_cpy = image.copy()
     image_cpy[:, :, 1] = image_cpy[:, :, 1] * delta
+
+    if np.any(image_cpy[:, :, 1] > 255):
+        print("Warning! Clipping by value 255")
+        image_cpy = np.clip(image_cpy, 0, 255)
 
     return image_cpy
 
@@ -19,8 +21,11 @@ def main(image_path: str | None=None):
     # Load image
     if image_path is None:
         image = astronaut()
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     else:
         image = cv2.imread(image_path)
+
+    assert len(image.shape) == 3 and image.shape[-1] == 3
 
     # Convert to HSV color palette
     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -40,8 +45,9 @@ def main(image_path: str | None=None):
         cv2.imshow("Higher S channel", image_new_higher_s)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
+            del image, image_hsv, image_hsv_lower_s, image_hsv_higher_s, image_new_lower_s, image_new_higher_s
             break
 
 
 if __name__ == "__main__":
-    main()
+    CLI(main, as_positional=False)
